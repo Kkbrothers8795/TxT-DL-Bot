3960fa24ae44bdef916c253338a9a497910ff8b1
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2025-08-20 20:59:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,9 @@ import re, os, time
 import datetime
 
 class Config:
-    API_ID = "1144902"
-    API_HASH ="e743e5a4f35076e4c558a4bd713082e9"
-    BOT_TOKEN = "7907920004:AAG18EpyWZKX27apayhahGDsQ8tB3NqBW-0"
+    BOT_TOKEN = "8067892925:AAF-5SXywyPgNJqLuXDA3jW8zqijbqUBR_Y"
+    API_HASH = "e743e5a4f35076e4c558a4bd713082e9"
+    API_ID = "e743e5a4f35076e4c558a4bd713082e9"
     OWNER_ID = "7788212890"
-    CREDIT = "kIRODWAL"
-    AUTH_USER = "7788212890"
     DB_URI = os.environ.get('DB_URI') #May Be In Future.
     
