46cbd2b0dac4f6c816044d4303771facf3aa2f8e
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2025-08-20 21:01:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import datetime
 class Config:
     BOT_TOKEN = "8067892925:AAF-5SXywyPgNJqLuXDA3jW8zqijbqUBR_Y"
     API_HASH = "e743e5a4f35076e4c558a4bd713082e9"
-    API_ID = "e743e5a4f35076e4c558a4bd713082e9"
+    API_ID = "1144902"
     OWNER_ID = "7788212890"
     DB_URI = os.environ.get('DB_URI') #May Be In Future.
     
